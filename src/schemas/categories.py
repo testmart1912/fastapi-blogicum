02711796d2fbb93bb5c171226ecc525da3f565ec
fifabetaversia1912@gmail.com <hayassalvador1912@gmail.com>
2026-02-26 22:08:52 +0400

    feat: add categories model

---
+++ src/schemas/categories.py
@@ -0,0 +1,7 @@
+from pydantic import Field
+from src.schemas.base import BasePublishedSchema, BaseCreatedAtSchema
+
+class CategorySchema(BasePublishedSchema, BaseCreatedAtSchema):
+    title: str = Field(max_length=256, description="Title")
+    description: str = Field(description="Description")
+    slug: str = Field(pattern=r'^[a-zA-Z0-9_-]+$', description="Identifier")
