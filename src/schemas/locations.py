394f6744de282bcadae596ed504ca4f43b698fae
fifabetaversia1912@gmail.com <hayassalvador1912@gmail.com>
2026-02-26 22:07:36 +0400

    feat: add locations model

---
+++ src/schemas/locations.py
@@ -0,0 +1,5 @@
+from pydantic import Field
+from src.schemas.base import BasePublishedSchema, BaseCreatedAtSchema
+
+class LocationSchema(BasePublishedSchema, BaseCreatedAtSchema):
+    name: str = Field(max_length=256, description="Name of the location")
