27943f1dbb676ffd66fa1fdd720192c1cb2434fe
fifabetaversia1912@gmail.com <hayassalvador1912@gmail.com>
2026-02-26 22:11:01 +0400

    feat: add posts model

---
+++ src/schemas/posts.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from pydantic import Field
+from pydantic_extra_types.mime_types import Image
+
+from src.schemas.users import UserSchema
+from src.schemas.locations import LocationSchema
+from src.schemas.categories import CategorySchema
+from src.schemas.base import BaseCreatedAtSchema, BasePublishedSchema
+
+
+class PostCreateSchema(BasePublishedSchema):
+    title: str = Field(max_length=256, description="Title")
+    text: str = Field(description="Text")
+    author: UserSchema = Field(description="Author")
+    pub_date: datetime = Field(default_factory=datetime.today, description="Date of publication")
+    location: LocationSchema = Field(description="Location")
+    category: CategorySchema = Field(description="Category")
+    image: Image
+
+class PostUpdateSchema(BasePublishedSchema):
+    title: str = Field(max_length=256, description="Title")
+    text: str = Field(description="Text")
+    location: LocationSchema = Field(description="Location")
+    category: CategorySchema = Field(description="Category")
+
+class PostResponseSchema(BaseCreatedAtSchema, BasePublishedSchema):
+    id: int = Field(description="ID")
+    title: str = Field(max_length=256, description="Title")
+    text: str = Field(description="Text")
+    author: UserSchema = Field(description="Author")
+    pub_date: datetime = Field(default_factory=datetime.today, description="Date of publication")
+    location: LocationSchema = Field(description="Location")
+    category: CategorySchema = Field(description="Category")
+    image: Image
