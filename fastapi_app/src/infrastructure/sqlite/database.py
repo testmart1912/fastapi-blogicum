6b90d8d78e1f347d3d553ec42b7bdd7ace831b76
fifabetaversia1912@gmail.com <hayassalvador1912@gmail.com>
2026-04-04 01:59:13 +0400

    fix: bug

--- fastapi_app/src/infrastructure/sqlite/database.py
+++ fastapi_app/src/infrastructure/sqlite/database.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Database:
     def __init__(self):
-        # Как в https://github.com/M1ssshka/fastapi-blogicum — корень репозитория и db.sqlite3
         base_dir = Path(__file__).resolve().parent.parent.parent.parent.parent
         db_path = base_dir / "db.sqlite3"
         self._db_url = f"sqlite:///{db_path}"
